67d7e8b85fc76f01c808970bf53fbe5bc31efb8f
BackflipPenguin <flippypenguin@protonmail.com>
2024-09-09 15:54:08 -0300

    change OUTPUT_URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@
 import Dashboard from '@/components/Dashboard';
 
 export default async function Page() {
-    const outputUrl = process.env.OUTPUT_URL || 'http://localhost:81/output';
+    const outputUrl = process.env.OUTPUT_URL;
     return <Dashboard outputUrl={outputUrl} />;
 }
